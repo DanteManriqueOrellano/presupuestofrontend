b4e2204ccf702278280279f1574802fdb8153309
DanteManriqueOrellano <relaxurbano@me.com>
2021-08-03 21:02:39 -0500

    modificando el servidor

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 //serve only the static files from the dist directory
-app.use(express.static('./dist/presupuestofontend'));
+app.use(express.static('./dist/presupuestofrontend'));
 app.get('/*', (req, res) =>
   res.sendFile('index.html', { root: 'dist/angular-heroku/' }),
 );
